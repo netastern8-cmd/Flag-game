1174b75258fb3449984c2d7e5291bd8a264acbe3
hadarman111-ctrl <hadrman111@gmail.com>
2026-08-16 15:33:39 +0300

    flag game

--- consts.py
+++ consts.py
@@ -2,12 +2,14 @@ import pygame
 
 BACKGROUND_MOVING=(34, 139, 34)
 BACKGROUND_FROZEN=(6, 10, 9)
-
+OG_SOLDIER=pygame.image.load("OGsoldier.png")
 FROZEN_SOLDIER = pygame.image.load("frozen_soldier.png")
 INJURED_SOLDIER = pygame.image.load("injured_soldier.png")
-GRASS_SOLDIER = pygame.image.load("grass.png")
+GRASS = pygame.image.load("grass.png")
 FLAG=pygame.image.load("flag.png")
 MINE=pygame.image.load("mine.png")
+SOLDIER_HEIGHT=4
+SOLDIER_WIDTH=2
 
 WIN_MESSAGE="you win!"
 LOSE_MESSAGE="you lose!"

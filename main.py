578fa100dcd0c56bd81b34f8de666d5eb77f046a
hadarman111-ctrl <hadrman111@gmail.com>
2026-08-16 15:36:53 +0300

    flag game

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import pygame
 import screen
+import soldier
 
 state={"is_soldier_on_mine":False,
        "is_soldier_on_flag":False,
-       "is_enter":False,
-       "is_window_open":True}
+       "enter_key_on":False,
+       "running":True,
+       "arrow_key_on":False,}
 
 
 def main():
-    run=True
-    while run:
+    while state["running"]:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run=False
+                state["running"]=False
 
-        screen.frozen_screen() #check
+       screen.frozen_screen() #check
     pygame.quit()
+
+'''
+def soldier_movmement(key):
+    keys_pressed = pygame.key.get_pressed()
+
+    if keys_pressed[pygame.K_RIGHT]:
+
+
+    if keys_pressed[pygame.K_LEFT]:
+
+    if keys_pressed[pygame.K_UP]:
+
+    if keys_pressed[pygame.K_DOWN]:
+'''
+
 
 if __name__ == "__main__":
     main()

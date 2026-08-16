b1c63ee20a5bd7f8907647215a5ddd938064c3fd
git exercise.py <meitaiiast@gmail.com>
2026-08-16 16:36:38 +0300

    marge

--- game_field.py
+++ game_field.py
@@ -4,6 +4,8 @@ import screen
 import random
 
 field=[]
+mine_lst=[]
+
 def create():
  global field
  field= [[0 for _ in range(25)] for _ in range(50)]
@@ -11,16 +13,16 @@ def create():
 
 
 def place_mines():
-    while len(screen.mine_lst)<20:
+    while len(mine_lst)<20:
         row,col=random.randint(0,24),random.randint(0,47)
-        if (row,col) not in screen.mine_lst:
-            screen.mine_lst.append((row,col))
+        if (row,col) not in mine_lst:
+            mine_lst.append((row,col))
             screen.drew_mine(row, col)
 
 def drew_mine_in_field(row,col):
     for row in field:
         for col in row:
-            if (row,col) in screen.mine_lst:
+            if (row,col) in mine_lst:
                 field[row][col].append("x")
 
 

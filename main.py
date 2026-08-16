b87d89341a25049b9680ad42b84e4e71bd773668
git exercise.py <meitaiiast@gmail.com>
2026-08-16 15:34:43 +0300

    marge

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pygame
 import screen
 
 
+
+
 def main():
     run=True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
-
-        screen.frozen_screen() #check
     pygame.quit()
 
 if __name__ == "__main__":

fa31a62f1bc6e6c7dd62f57e054aa7c7965e91a5
git exercise.py <meitaiiast@gmail.com>
2026-08-16 15:33:25 +0300

    consts marge

--- consts.py
+++ consts.py
@@ -2,15 +2,23 @@ import pygame
 
 BACKGROUND_MOVING=(34, 139, 34)
 BACKGROUND_FROZEN=(6, 10, 9)
+WIDTH=1000
+HEIGHT=WIDTH//2
+BLOCK_SIZE=WIDTH//50
+
+
+LINE_COLOR=(17, 101, 102)
 
 FROZEN_SOLDIER = pygame.image.load("frozen_soldier.png")
 INJURED_SOLDIER = pygame.image.load("injured_soldier.png")
-GRASS_SOLDIER = pygame.image.load("grass.png")
+GRASS = pygame.image.load("grass.png")
 FLAG=pygame.image.load("flag.png")
 MINE=pygame.image.load("mine.png")
 
 WIN_MESSAGE="you win!"
 LOSE_MESSAGE="you lose!"
+
+FPS=60
 
 
 

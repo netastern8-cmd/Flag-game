3840252c456747ede76337b333ad679e5be9b9ec
git exercise.py <meitaiiast@gmail.com>
2026-08-17 15:19:03 +0300

    dbuging

--- movement.py
+++ movement.py
@@ -2,17 +2,21 @@ import screen
 import game_field
 
 def move_soldier_right(row,col):
+   screen.moving_screen()
    screen.draw_OG_soldier(row,col+1)
    game_field.mark_soldier_position(row,col+1)
 
 def move_soldier_left(row,col):
+    screen.moving_screen()
     screen.draw_OG_soldier(row,col-1)
     game_field.mark_soldier_position(row,col-1)
 
 def move_soldier_up(row,col):
+    screen.moving_screen()
     screen.draw_OG_soldier(row+1,col)
     game_field.mark_soldier_position(row+1,col)
 
 def move_soldier_down(row,col):
+    screen.moving_screen()
     screen.draw_OG_soldier(row-1,col)
     game_field.mark_soldier_position(row-1,col)
